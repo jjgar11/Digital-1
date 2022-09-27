9481b8abb906daf69cdb014283a565794fe2f3b3
Juan Garcia <juanjgarcialeon@gmail.com>
2022-09-27 05:42:55 -0500

    PWM terminado.

--- PWM/PWM.vhd
+++ PWM/PWM.vhd
@@ -8,12 +8,12 @@ entity PWM is
 
 	port(
 		-- input ports
-		Up : in std_logic;
-		Down : in std_logic;
+		nUp : in std_logic;
+		nDown : in std_logic;
 		-- clk : in std_logic;
 
 		-- output ports
-		led : out std_logic
+		Led : out std_logic
 	);
 
 end PWM;
@@ -23,19 +23,19 @@ architecture Behavioral of PWM is
 	-- * Declare las señales que considere necesarias. Estas señales saldrán de unas instancias y entrarán
 	-- a otras.
 	
-	signal cuenta : integer range 1 to 10;
-	-- signal cuenta2 : integer range 1 to 25000;
+	signal Up : std_logic;
+	signal Down : std_logic;
 
+	-- Se simula el clock de la FPGA
 	constant ClockFrequency : integer := 50e6; -- 50 MHz
     constant ClockPeriod    : time    := 1000 ms / ClockFrequency;
+
 	constant T : integer := 10;
 
-	signal clk : std_logic := '1';
-	signal nclk : std_logic := '1';
-	signal clk_div : std_logic := '1';
-	signal sled : std_logic := '1';
+	signal clk, nclk, clk_div : std_logic := '1';
+	signal sled : std_logic := '0';
 	signal tau : integer range 0 to 10 := 2;
-	signal tau_mod : integer range 0 to 10;
+	signal increase, decrease : std_logic := '0';
 
 	-- * Declare el componente del divisor de frecuencia y del antirrebote.
 	
@@ -45,14 +45,23 @@ architecture Behavioral of PWM is
 			Nciclos: in	integer;
 			
 			f: out std_logic
-			);
-		end component;
+		);
+	end component;
+
+	component anti_rebote
+		port(
+			clk : in std_logic;
+			Button : in std_logic;
+			isOn : out std_logic
+		);
+	end component;
 		
 	-- * Declare el componente del modulador y de la señal PWM.
 	
 	component Modulador
 		port(
 			-- input ports
+			clk : in std_logic;
 			Up : in std_logic;
 			Down : in std_logic;
 			tau : in integer range 0 to 10;
@@ -75,20 +84,33 @@ architecture Behavioral of PWM is
 
 begin
 
+	-- En simulacion
 	clk <= not clk after ClockPeriod / 2;
+	Up <= nUp;
+	Down <= nDown;
+
+	-- En FPGA
+	-- Up <= not nUp;
+	-- Down <= not nDown;
 
 	-- * Cree instancias del divisor de frecuencia para generar la señal de muestreo de los antirrebote
 	--   y la señal de reloj del registro que almacenará el conteo del pwm.
 
 	div_pulsador : div_frec
-	port map(clk,25,clk_div);
+	port map(clk,2500,clk_div);
 
 	-- * Cree instancias del antirrebote para el pulsador de aumento y de reducción de tau.
+
+	reboteUp : anti_rebote
+	port map(clk_div,Up,increase);
+	
+	reboteDown : anti_rebote
+	port map(clk_div,Down,decrease);
 
 	-- * Cree la instancia del modulador y de la señal PWM.
 
 	modtau : Modulador
-	port map(Up,Down,tau,tau);
+	port map(clk_div,increase,decrease,tau,tau);
 
 	div_1Hz : div_frec
 	port map(clk,25e3,nclk);
@@ -96,33 +118,9 @@ begin
 	senal : senal_PWM
 	port map(tau,T,nclk,sled);
 
-	process(clk, nclk)
-
-	begin
-		-- if rising_edge(clk) then
-		-- 	if cuenta2 = 25 then
-		-- 		nclk <= not nclk;
-		-- 		cuenta2 <= 1;
-		-- 	else
-		-- 		cuenta2 <= cuenta + 1;
-		-- 	end if;
-		-- end if;
-		-- if rising_edge(nclk) then
-			-- if cuenta = 10 then
-			-- 	cuenta <= 1;
-			-- 	sled <= not sled;
-			-- else
-			-- 	cuenta <= cuenta + 1;
-			-- end if;
-			-- if cuenta = t then
-			-- 	sled <= not sled;
-			-- end if;
-		-- end if;
-
-	end process;
-
 	-- * Conecte las instancias con las señales internas (intermedias) que declaró.
 
-	led <= sled;
+	-- Led <= not sled;			-- En FPGA
+	Led <= sled;				-- En simulacion
 
 end Behavioral;
